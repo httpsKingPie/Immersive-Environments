16691723e6d62fcc38cbf6f5f0a26a1ecb7889d7
httpsKingPie <54006975+httpsKingPie@users.noreply.github.com>
2021-01-04 12:14:09 -0500

    1.279
    Added example weather code in Activation

--- src/ServerScriptService/Activator.server.lua
+++ src/ServerScriptService/Activator.server.lua
@@ -4,3 +4,22 @@ local IEFolder = ReplicatedStorage.IE
 local IEMain = require(IEFolder.Main)
 
 IEMain.Run()
+
+--[[
+
+Example for weather settings
+
+wait(5)
+
+local AudioHandling = require(IEFolder.Main.AudioHandling)
+local LightingHandling = require(IEFolder.Main.LightingHandling)
+
+AudioHandling.ChangeWeather("TestWeather")
+LightingHandling.ChangeWeather("TestWeather")
+
+wait(12)
+
+AudioHandling.ClearWeather()
+LightingHandling.ClearWeather()
+
+]]
